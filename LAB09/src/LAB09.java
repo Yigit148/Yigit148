249da99fac1e017d33e76a59d8b1a53d787a3163
Yiğit Can Yöntem <69350457+Yigit148@users.noreply.github.com>
2022-05-17 15:03:45 +0300

    Add files via upload

---
+++ LAB09/src/LAB09.java
@@ -0,0 +1,104 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class LAB09 {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("\n" + "Part 1");
+        ArrayList list = readBooks("books.txt");
+        readBooks("books.txt");
+        printBooks(list);
+
+        System.out.println("\n" + "Part 2");
+        System.out.println("Enter part of author name:");
+        String auth_name = scanner.nextLine();
+        System.out.println("Books written by " + auth_name);
+        findBooks(list,auth_name);
+
+        System.out.println("\n" + "Part 3");
+        System.out.println("Enter an integer number:");
+        int num = scanner.nextInt();
+        System.out.println("Sum of digits of " + num + " is " + sumDigits(num));
+
+    }
+
+    public static ArrayList readBooks(String filename) throws FileNotFoundException {
+        Scanner fileScan = new Scanner(new File(filename));
+        ArrayList<Object> lst = new ArrayList<>();
+
+        while (fileScan.hasNext()) {
+            String x = fileScan.nextLine();
+            String[] values = x.split(";");
+            int i = Integer.parseInt(values[2]);
+            int j = Integer.parseInt(values[3]);
+            Book names = new Book(values[0], values[1], i, j);
+            lst.add(names);
+        }
+        return lst;
+    }
+
+    public static void printBooks (ArrayList Arr) {
+        for (Object x : Arr) {
+            System.out.println(x.toString());
+        }
+    }
+
+    public static void findBooks(ArrayList Arr, String author_name) {
+        int count = 0;
+        for (Object x : Arr ){
+            String str = x + "";
+            if (str.contains(author_name)){
+                System.out.println(Arr.get(count));
+            }
+            count++;
+        }
+    }
+
+    public static int sumDigits(int num){
+        if (num == 0)
+            return 0;
+        return (num % 10 + sumDigits(num / 10));
+    }
+
+
+}
+
+class Book {
+    private String name;
+    private String author;
+    private int year;
+    private int pages;
+
+    public Book(String name, String author, int year, int pages) {
+        this.name = name;
+        this.author = author;
+        this.year = year;
+        this.pages = pages;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;}
+
+    public void setPage(int pages) {
+        this.pages = pages;}
+
+    public String getName() {
+        return name;}
+
+    public String getAuthor() {
+        return author;}
+
+    public int getYear() {
+        return year;}
+
+    public int getPages() {
+        return pages;}
+
+    public String toString() {
+        return name + ", " + author + ", " + year + ", " + pages + " p.";
+    }
+
+}
